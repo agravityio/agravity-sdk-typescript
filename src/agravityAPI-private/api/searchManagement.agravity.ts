09caa08f4c0dacd9e78a2d361043056000db84c2
Philipp Losbichler <philipp.losbichler@agravity.io>
2024-04-07 22:04:38 +0200

    - Add PATCH `/searchadmin/clean` - This endpoint cleans the deleted assets from the search index (with param `portal_id` the portal gets cleared).

--- src/agravityAPI-private/api/searchManagement.agravity.ts
+++ src/agravityAPI-private/api/searchManagement.agravity.ts
@@ -364,6 +364,65 @@ export class SearchManagementService {
 	}
 
 	/**
+	 * This endpoint removes all assets which are not longer available in the database from the search index.
+	 * @param portalId If the search should be redirected to a specific portal.
+	 * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+	 * @param reportProgress flag to report request and response progress.
+	 */
+	public httpPatchCleanSearchFromDeletedAssets(portalId?: string, observe?: 'body', reportProgress?: boolean, options?: { httpHeaderAccept?: 'application/json' }): Observable<AgravityInfoResponse>;
+	public httpPatchCleanSearchFromDeletedAssets(
+		portalId?: string,
+		observe?: 'response',
+		reportProgress?: boolean,
+		options?: { httpHeaderAccept?: 'application/json' }
+	): Observable<HttpResponse<AgravityInfoResponse>>;
+	public httpPatchCleanSearchFromDeletedAssets(
+		portalId?: string,
+		observe?: 'events',
+		reportProgress?: boolean,
+		options?: { httpHeaderAccept?: 'application/json' }
+	): Observable<HttpEvent<AgravityInfoResponse>>;
+	public httpPatchCleanSearchFromDeletedAssets(portalId?: string, observe: any = 'body', reportProgress: boolean = false, options?: { httpHeaderAccept?: 'application/json' }): Observable<any> {
+		let queryParameters = new HttpParams({ encoder: this.encoder });
+		if (portalId !== undefined && portalId !== null) {
+			queryParameters = this.addToHttpParams(queryParameters, <any>portalId, 'portal_id');
+		}
+
+		let headers = this.defaultHeaders;
+
+		let credential: string | undefined;
+		// authentication (msal_auth) required
+		credential = this.configuration.lookupCredential('msal_auth');
+		if (credential) {
+			headers = headers.set('Authorization', 'Bearer ' + credential);
+		}
+
+		let httpHeaderAcceptSelected: string | undefined = options && options.httpHeaderAccept;
+		if (httpHeaderAcceptSelected === undefined) {
+			// to determine the Accept header
+			const httpHeaderAccepts: string[] = ['application/json'];
+			httpHeaderAcceptSelected = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+		}
+		if (httpHeaderAcceptSelected !== undefined) {
+			headers = headers.set('Accept', httpHeaderAcceptSelected);
+		}
+
+		let responseType_: 'text' | 'json' = 'json';
+		if (httpHeaderAcceptSelected && httpHeaderAcceptSelected.startsWith('text')) {
+			responseType_ = 'text';
+		}
+
+		return this.httpClient.patch<AgravityInfoResponse>(`${this.configuration.basePath}/searchadmin/clean`, null, {
+			params: queryParameters,
+			responseType: <any>responseType_,
+			withCredentials: this.configuration.withCredentials,
+			headers: headers,
+			observe: observe,
+			reportProgress: reportProgress
+		});
+	}
+
+	/**
 	 * This endpoint gives the status about the index, indexes and data source connection.
 	 * @param portalId If the search should be redirected to a specific portal.
 	 * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
