82d42acf162a603d06eed44ae9423b553e0bfbc3
Julian Richtsfeld <jri@agravity.io>
2024-11-12 10:56:17 +0100

    #1027 Add edit endpoint for shared collection

--- src/agravityAPI-private/api/collectionShareManagement.agravity.ts
+++ src/agravityAPI-private/api/collectionShareManagement.agravity.ts
@@ -54,6 +54,13 @@ export interface HttpSharedCollectionsGetByIdRequestParams {
 	acceptLanguage?: string;
 }
 
+export interface HttpSharedCollectionsUpdateByIdRequestParams {
+	/** The ID of the shared collection. */
+	id: string;
+	/** This updated shared collection to put in DB. */
+	sharedCollection: SharedCollection;
+}
+
 @Injectable({
 	providedIn: 'root'
 })
@@ -489,6 +496,105 @@ export class CollectionShareManagementService {
 		return this.httpClient.request<SharedCollectionFull>('get', `${this.configuration.basePath}${localVarPath}`, {
 			context: localVarHttpContext,
 			params: localVarQueryParameters,
+			responseType: <any>responseType_,
+			withCredentials: this.configuration.withCredentials,
+			headers: localVarHeaders,
+			observe: observe,
+			transferCache: localVarTransferCache,
+			reportProgress: reportProgress
+		});
+	}
+
+	/**
+	 * Updates an existing shared collection.
+	 * @param requestParameters
+	 * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+	 * @param reportProgress flag to report request and response progress.
+	 */
+	public httpSharedCollectionsUpdateById(
+		requestParameters?: HttpSharedCollectionsUpdateByIdRequestParams,
+		observe?: 'body',
+		reportProgress?: boolean,
+		options?: { httpHeaderAccept?: 'application/json'; context?: HttpContext; transferCache?: boolean }
+	): Observable<SharedCollection>;
+	public httpSharedCollectionsUpdateById(
+		requestParameters?: HttpSharedCollectionsUpdateByIdRequestParams,
+		observe?: 'response',
+		reportProgress?: boolean,
+		options?: { httpHeaderAccept?: 'application/json'; context?: HttpContext; transferCache?: boolean }
+	): Observable<HttpResponse<SharedCollection>>;
+	public httpSharedCollectionsUpdateById(
+		requestParameters?: HttpSharedCollectionsUpdateByIdRequestParams,
+		observe?: 'events',
+		reportProgress?: boolean,
+		options?: { httpHeaderAccept?: 'application/json'; context?: HttpContext; transferCache?: boolean }
+	): Observable<HttpEvent<SharedCollection>>;
+	public httpSharedCollectionsUpdateById(
+		requestParameters?: HttpSharedCollectionsUpdateByIdRequestParams,
+		observe: any = 'body',
+		reportProgress: boolean = false,
+		options?: { httpHeaderAccept?: 'application/json'; context?: HttpContext; transferCache?: boolean }
+	): Observable<any> {
+		const id = requestParameters?.id;
+		if (id === null || id === undefined) {
+			throw new Error('Required parameter id was null or undefined when calling httpSharedCollectionsUpdateById.');
+		}
+		const sharedCollection = requestParameters?.sharedCollection;
+		if (sharedCollection === null || sharedCollection === undefined) {
+			throw new Error('Required parameter sharedCollection was null or undefined when calling httpSharedCollectionsUpdateById.');
+		}
+
+		let localVarHeaders = this.defaultHeaders;
+
+		let localVarCredential: string | undefined;
+		// authentication (msal_auth) required
+		localVarCredential = this.configuration.lookupCredential('msal_auth');
+		if (localVarCredential) {
+			localVarHeaders = localVarHeaders.set('Authorization', 'Bearer ' + localVarCredential);
+		}
+
+		let localVarHttpHeaderAcceptSelected: string | undefined = options && options.httpHeaderAccept;
+		if (localVarHttpHeaderAcceptSelected === undefined) {
+			// to determine the Accept header
+			const httpHeaderAccepts: string[] = ['application/json'];
+			localVarHttpHeaderAcceptSelected = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+		}
+		if (localVarHttpHeaderAcceptSelected !== undefined) {
+			localVarHeaders = localVarHeaders.set('Accept', localVarHttpHeaderAcceptSelected);
+		}
+
+		let localVarHttpContext: HttpContext | undefined = options && options.context;
+		if (localVarHttpContext === undefined) {
+			localVarHttpContext = new HttpContext();
+		}
+
+		let localVarTransferCache: boolean | undefined = options && options.transferCache;
+		if (localVarTransferCache === undefined) {
+			localVarTransferCache = true;
+		}
+
+		// to determine the Content-Type header
+		const consumes: string[] = ['application/json'];
+		const httpContentTypeSelected: string | undefined = this.configuration.selectHeaderContentType(consumes);
+		if (httpContentTypeSelected !== undefined) {
+			localVarHeaders = localVarHeaders.set('Content-Type', httpContentTypeSelected);
+		}
+
+		let responseType_: 'text' | 'json' | 'blob' = 'json';
+		if (localVarHttpHeaderAcceptSelected) {
+			if (localVarHttpHeaderAcceptSelected.startsWith('text')) {
+				responseType_ = 'text';
+			} else if (this.configuration.isJsonMime(localVarHttpHeaderAcceptSelected)) {
+				responseType_ = 'json';
+			} else {
+				responseType_ = 'blob';
+			}
+		}
+
+		let localVarPath = `/shared/${this.configuration.encodeParam({ name: 'id', value: id, in: 'path', style: 'simple', explode: false, dataType: 'string', dataFormat: undefined })}`;
+		return this.httpClient.request<SharedCollection>('post', `${this.configuration.basePath}${localVarPath}`, {
+			context: localVarHttpContext,
+			body: sharedCollection,
 			responseType: <any>responseType_,
 			withCredentials: this.configuration.withCredentials,
 			headers: localVarHeaders,
